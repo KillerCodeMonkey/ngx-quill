e941e484b803010367f5313f8e6e3218378ac55e
Bengt Weiße <bengtler@gmail.com>
2017-01-03 10:26:16 +0100

    Remove unused modules

--- src/quill/quill.module.ts
+++ src/quill/quill.module.ts
@@ -1,7 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { QuillEditorComponent } from './quill-editor.component';
 
