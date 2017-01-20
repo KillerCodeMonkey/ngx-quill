34f24cbd94ff8f17f8936326f53a6950b8a79cc6
Bengt Weiße <bengtler@gmail.com>
2017-01-20 08:34:24 +0100

    Fix realem, move example to own project nix-quill-example

--- example/dist/src/app.component.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AppComponent {
-    title: string;
-    isReadOnly: boolean;
-    toggleReadOnly(): void;
-    logChange($event: any): void;
-}
--- example/src/main.ts
+++
@@ -1,6 +0,0 @@
-import './polyfills.ts';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
