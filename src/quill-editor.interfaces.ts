c12f3d4f8b24e9e9266387d2014e4e02de5b90c6
Bengt Weiße <bengtler@gmail.com>
2018-11-20 07:45:46 +0100

    style(Interfaces): indention

--- src/quill-editor.interfaces.ts
+++ src/quill-editor.interfaces.ts
@@ -1,20 +1,20 @@
 export type QuillToolbarConfig = ((string | {
-    indent?: string;
-    list?: string;
-    direction?: string;
-    header?: number | (boolean | number)[];
-    color?: string[];
-    background?: string[];
-    align?: string[];
-    script?: string;
-    font?: string[];
-    size?: (boolean | string)[];
+  indent?: string
+  list?: string
+  direction?: string
+  header?: number | (boolean | number)[]
+  color?: string[]
+  background?: string[]
+  align?: string[]
+  script?: string
+  font?: string[]
+  size?: (boolean | string)[]
 })[])[];
 
 export type QuillModules = {
-    toolbar: QuillToolbarConfig;
-}
+  toolbar: QuillToolbarConfig
+};
 
 export interface QuillConfig {
-    modules: QuillModules;
+  modules: QuillModules;
 }
