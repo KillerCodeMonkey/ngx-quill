42bafa88e0c631e928cc87daa88d69c30065fcdb
Bengt Weiße <bengtler@gmail.com>
2019-02-08 09:33:35 +0100

    feat: defaults in extra file

---
+++ src/quill-defaults.ts
@@ -0,0 +1,26 @@
+export const defaultModules = {
+  toolbar: [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+    [{ direction: 'rtl' }], // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [
+      { color: [] },
+      { background: [] }
+    ], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ['clean'], // remove formatting button
+
+    ['link', 'image', 'video'] // link and image, video
+  ]
+}
