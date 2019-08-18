ec700ddd7bd154ed5eb4b6d79741161c135a8a18
Bengt Weiße <bengtler@gmail.com>
2019-08-18 13:51:43 +0200

    update deps  corejs 3

--- karma-shim.js
+++ karma-shim.js
@@ -1,7 +1,7 @@
 Error.stackTraceLimit = Infinity
 
-require('core-js/es6')
-require('core-js/es7/reflect')
+require('core-js/es')
+require('core-js/proposals/reflect-metadata')
 
 require('zone.js/dist/zone')
 require('zone.js/dist/long-stack-trace-zone')
