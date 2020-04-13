7d62334fca6ac4974af8b71d1b67d97a40c837f8
Bengt Weiße <bengtler@gmail.com>
2020-04-13 12:49:09 +0200

    chore: replace karma with jest

---
+++ src/setupJest.ts
@@ -0,0 +1,16 @@
+import 'jest-preset-angular'
+
+// (document as any).getSelection = () => {
+//   return {
+//     addRange: () => {},
+//     createRange: () => {},
+//     getRangeAt: () => {},
+//     removeAllRanges: () => {}
+//   }
+// }
+// (document as any).createRange = () => {
+//   return {
+//     setEnd: () => {},
+//     setStart: () => {}
+//   }
+// }
