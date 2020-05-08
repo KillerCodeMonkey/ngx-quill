d1382f2de9a0af044a9dbdca2d924c9650d68314
Bengt Weiße <bengtler@gmail.com>
2020-05-08 08:56:54 +0200

    refactor: angular cli lib project

---
+++ projects/ngx-quill/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of ngx-quill
+ */
+
+export * from './lib/quill.module'
+export * from './lib/quill-defaults'
+export * from './lib/quill-editor.component'
+export * from './lib/quill-view.component'
+export * from './lib/quill-view-html.component'
+export * from './lib/quill-editor.interfaces'
