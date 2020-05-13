13f651ad67c3bbadc13549155c08a823a575081f
Josh Pike <josh@streaka.com>
2020-05-13 20:41:39 +0800

    Update quill-view.component.ts

--- projects/ngx-quill/src/lib/quill-view.component.ts
+++ projects/ngx-quill/src/lib/quill-view.component.ts
@@ -87,7 +87,7 @@ export class QuillViewComponent implements AfterViewInit, OnChanges {
 
     const Quill = await this.service.getQuill()
 
-    const modules = this.service.config.modules;
+    const modules = this.service.config.modules
     modules.toolbar = false
 
     this.customOptions.forEach((customOption) => {
