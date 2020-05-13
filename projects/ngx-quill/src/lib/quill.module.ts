aca13620772d8aebf1f6bf8e4463da1e919e9d60
Josh Pike <josh@streaka.com>
2020-05-13 20:29:45 +0800

    fix

--- projects/ngx-quill/src/lib/quill.module.ts
+++ projects/ngx-quill/src/lib/quill.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { ModuleWithProviders, NgModule } from '@angular/core'
 
-import { defaultModules } from './quill-defaults'
 import { QuillEditorComponent } from './quill-editor.component'
 import { QUILL_CONFIG_TOKEN, QuillConfig } from './quill-editor.interfaces'
 import { QuillViewHTMLComponent } from './quill-view-html.component'
@@ -20,19 +19,12 @@ import { QuillService } from './quill.service'
 })
 export class QuillModule {
   static forRoot(config?: QuillConfig): ModuleWithProviders<QuillModule> {
-    if (config) {
-      // set default modules as modules if not modules key passed on custom config
-      if (!config.modules) {
-        config.modules = defaultModules
-      }
-    }
-
     return {
       ngModule: QuillModule,
       providers: [
         {
           provide: QUILL_CONFIG_TOKEN,
-          useValue: config || { modules: defaultModules }
+          useValue: config
         }
       ]
     }
