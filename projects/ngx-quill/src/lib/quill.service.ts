b2ab1ae8dd87d19a131e10184db0d41a49229d9a
arturovt <arthurandrosovich@gmail.com>
2021-06-30 21:44:12 +0300

    perf: use `DOCUMENT` token to retrieve the token

--- projects/ngx-quill/src/lib/quill.service.ts
+++ projects/ngx-quill/src/lib/quill.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, Inject } from '@angular/core'
-import { QUILL_CONFIG_TOKEN, QuillConfig } from './quill-editor.interfaces'
+import { DOCUMENT } from '@angular/common'
+import { Injectable, Inject, Injector } from '@angular/core'
+
 import { defaultModules } from './quill-defaults'
+import { QUILL_CONFIG_TOKEN, QuillConfig } from './quill-editor.interfaces'
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,14 @@ export class QuillService {
   private Quill!: any
   private $importPromise!: Promise<any>
   private count = 0
+  private document: Document
 
   constructor(
+    injector: Injector,
     @Inject(QUILL_CONFIG_TOKEN) public config: QuillConfig
   ) {
+    this.document = injector.get(DOCUMENT)
+
     if (!this.config) {
       this.config = { modules: defaultModules }
     }
@@ -26,7 +32,7 @@ export class QuillService {
         // Quill adds events listeners on import https://github.com/quilljs/quill/blob/develop/core/emitter.js#L8
         // We'd want to use the unpatched `addEventListener` method to have all event callbacks to be run outside of zone.
         // We don't know yet if the `zone.js` is used or not, just save the value to restore it back further.
-        const maybePatchedAddEventListener = document.addEventListener
+        const maybePatchedAddEventListener = this.document.addEventListener
         // There're 2 types of Angular applications:
         // 1) zone-full (by default)
         // 2) zone-less
@@ -37,9 +43,9 @@ export class QuillService {
         // The `__zone_symbol__addEventListener` is basically a native DOM API, which is not patched by zone.js, thus not even going
         // through the `zone.js` task lifecycle. You can also access the native DOM API as follows `target[Zone.__symbol__('methodName')]`.
         // eslint-disable-next-line @typescript-eslint/dot-notation
-        document.addEventListener = document['__zone_symbol__addEventListener'] || document.addEventListener
+        this.document.addEventListener = this.document['__zone_symbol__addEventListener'] || this.document.addEventListener
         const quillImport = await import('quill')
-        document.addEventListener = maybePatchedAddEventListener
+        this.document.addEventListener = maybePatchedAddEventListener
 
         this.Quill = (quillImport.default ? quillImport.default : quillImport) as any
 
