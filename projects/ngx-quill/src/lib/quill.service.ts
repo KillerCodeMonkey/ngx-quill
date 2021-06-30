80fa3adba1ddc46ee2ae1e4cb26bce9548553d53
arturovt <arthurandrosovich@gmail.com>
2021-06-30 13:37:36 +0300

    perf: unpatch `document.addEventListener` when the `quill` is imported

--- projects/ngx-quill/src/lib/quill.service.ts
+++ projects/ngx-quill/src/lib/quill.service.ts
@@ -23,7 +23,23 @@ export class QuillService {
     this.count++
     if (!this.Quill && this.count === 1) {
       this.$importPromise = new Promise(async (resolve) => {
+        // Quill adds events listeners on import https://github.com/quilljs/quill/blob/develop/core/emitter.js#L8
+        // We'd want to use the unpatched `addEventListener` method to have all event callbacks to be run outside of zone.
+        // We don't know yet if the `zone.js` is used or not, just save the value to restore it back further.
+        const maybePatchedAddEventListener = document.addEventListener
+        // There're 2 types of Angular applications:
+        // 1) zone-full (by default)
+        // 2) zone-less
+        // The developer can avoid importing the `zone.js` package and tells Angular that he/she is responsible for running
+        // the change detection by himself. This is done by "nooping" the zone through `CompilerOptions` when bootstrapping
+        // the root module. We fallback to `document.addEventListener` if `__zone_symbol__addEventListener` is not defined,
+        // this means the `zone.js` is not imported.
+        // The `__zone_symbol__addEventListener` is basically a native DOM API, which is not patched by zone.js, thus not even going
+        // through the `zone.js` task lifecycle. You can also access the native DOM API as follows `target[Zone.__symbol__('methodName')]`.
+        // eslint-disable-next-line @typescript-eslint/dot-notation
+        document.addEventListener = document['__zone_symbol__addEventListener'] || document.addEventListener
         const quillImport = await import('quill')
+        document.addEventListener = maybePatchedAddEventListener
 
         this.Quill = (quillImport.default ? quillImport.default : quillImport) as any
 
