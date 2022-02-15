146a14841a8e296ec6e1004c9c86f4dbdbbc489d
Bengt Weiße <bengtler@gmail.com>
2022-02-15 13:15:35 +0100

    fix: try fixing overriding the quill instance

--- projects/ngx-quill/src/lib/quill.service.ts
+++ projects/ngx-quill/src/lib/quill.service.ts
@@ -14,27 +14,29 @@ export class QuillService {
   private Quill!: any
   private document: Document
   private quill$: Observable<any> = defer(async () => {
-    // Quill adds events listeners on import https://github.com/quilljs/quill/blob/develop/core/emitter.js#L8
-    // We'd want to use the unpatched `addEventListener` method to have all event callbacks to be run outside of zone.
-    // We don't know yet if the `zone.js` is used or not, just save the value to restore it back further.
-    const maybePatchedAddEventListener = this.document.addEventListener
-    // There're 2 types of Angular applications:
-    // 1) zone-full (by default)
-    // 2) zone-less
-    // The developer can avoid importing the `zone.js` package and tells Angular that he/she is responsible for running
-    // the change detection by himself. This is done by "nooping" the zone through `CompilerOptions` when bootstrapping
-    // the root module. We fallback to `document.addEventListener` if `__zone_symbol__addEventListener` is not defined,
-    // this means the `zone.js` is not imported.
-    // The `__zone_symbol__addEventListener` is basically a native DOM API, which is not patched by zone.js, thus not even going
-    // through the `zone.js` task lifecycle. You can also access the native DOM API as follows `target[Zone.__symbol__('methodName')]`.
-    // eslint-disable-next-line @typescript-eslint/dot-notation
-    this.document.addEventListener = this.document['__zone_symbol__addEventListener'] || this.document.addEventListener
-    const quillImport = await import('quill')
-    this.document.addEventListener = maybePatchedAddEventListener
+    if (!this.Quill) {
+      // Quill adds events listeners on import https://github.com/quilljs/quill/blob/develop/core/emitter.js#L8
+      // We'd want to use the unpatched `addEventListener` method to have all event callbacks to be run outside of zone.
+      // We don't know yet if the `zone.js` is used or not, just save the value to restore it back further.
+      const maybePatchedAddEventListener = this.document.addEventListener
+      // There're 2 types of Angular applications:
+      // 1) zone-full (by default)
+      // 2) zone-less
+      // The developer can avoid importing the `zone.js` package and tells Angular that he/she is responsible for running
+      // the change detection by himself. This is done by "nooping" the zone through `CompilerOptions` when bootstrapping
+      // the root module. We fallback to `document.addEventListener` if `__zone_symbol__addEventListener` is not defined,
+      // this means the `zone.js` is not imported.
+      // The `__zone_symbol__addEventListener` is basically a native DOM API, which is not patched by zone.js, thus not even going
+      // through the `zone.js` task lifecycle. You can also access the native DOM API as follows `target[Zone.__symbol__('methodName')]`.
+      // eslint-disable-next-line @typescript-eslint/dot-notation
+      this.document.addEventListener = this.document['__zone_symbol__addEventListener'] || this.document.addEventListener
+      const quillImport = await import('quill')
+      this.document.addEventListener = maybePatchedAddEventListener
 
-    this.Quill = (
-      quillImport.default ? quillImport.default : quillImport
-    ) as any
+      this.Quill = (
+        quillImport.default ? quillImport.default : quillImport
+      ) as any
+    }
 
     // Only register custom options and modules once
     this.config.customOptions?.forEach((customOption) => {
