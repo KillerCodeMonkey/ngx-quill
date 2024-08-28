ec2d5ccd1ecbf86dbf27cc174c5236ea318947e0
BBBoy01 <1156678002@qq.com>
2024-08-28 11:52:02 +0800

    chore: migrate eslint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,92 @@
+// @ts-check
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
+
+import angular from 'angular-eslint'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+
+  {
+    name: 'tsc options',
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['tsconfig.json', 'e2e/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+
+  {
+    name: 'global ts file rules',
+    files: ['**/*.ts'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    extends: [...tseslint.configs.recommended],
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/member-delimiter-style': [
+        'error',
+        {
+          multiline: {
+            delimiter: 'none',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'semi',
+            requireLast: false,
+          },
+          multilineDetection: 'brackets',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          filter: {
+            regex: '^Quill$',
+            match: false,
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    name: 'angular ts recommended rules',
+    files: ['**/*.ts'],
+    extends: [...angular.configs.tsRecommended],
+    processor: angular.processInlineTemplates,
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@angular-eslint/no-output-on-prefix': 'off',
+    },
+  },
+
+  {
+    name: 'angular template recommended rules',
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+      '@angular-eslint/template/no-negated-async': 'off',
+    },
+  },
+)
