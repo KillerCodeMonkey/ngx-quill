c951cb44b7f1230ca6c38cf66022394735101b85
KillerCodeMonkey <bengtler@gmail.com>
2025-06-13 08:23:47 +0200

    chore: deps + cleanup testing setup

--- projects/ngx-quill/src/test.ts
+++ projects/ngx-quill/src/test.ts
@@ -1,22 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import { getTestBed } from '@angular/core/testing'
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing'
 import 'zone.js'
 import 'zone.js/testing'
-import { getTestBed } from '@angular/core/testing'
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T
-    keys(): string[]
-  }
-}
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  BrowserTestingModule,
+  platformBrowserTesting()
 )
+
