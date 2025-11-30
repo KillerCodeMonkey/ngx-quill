5e8747237d190a44952cdfb3b1158f6bc58397c7
Bengt Weiße <bengtler@gmail.com>
2025-11-30 11:30:43 +0100

    chore: cleanup test-setup

--- projects/ngx-quill/test-setup.ts
+++ projects/ngx-quill/test-setup.ts
@@ -1,12 +1,6 @@
 import { getTestBed } from '@angular/core/testing'
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing'
 
-import { afterEach, vi } from 'vitest'
-
-afterEach(() => {
-  vi.clearAllTimers()
-})
-
 getTestBed().initTestEnvironment(
   BrowserTestingModule,
   platformBrowserTesting()
